d183ac197e503bb42b7bb40316c2b2476fd2ef17
Eduardo Tolino <eduardo.tolino@tolitech.com.br>
2025-07-27 15:25:51 -0300

    Initial commit.

---
+++ src/Presentation.API.Generators.Abstractions/IEndpoint.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace Tolitech.Presentation.API.Generators.Abstractions;
+
+/// <summary>
+/// Defines a contract for mapping an endpoint to an <see cref="IEndpointRouteBuilder"/>.
+/// </summary>
+/// <remarks>Implementations of this interface should provide the logic to configure and map specific endpoints
+/// within an application's routing system. This is typically used to set up routes for handling HTTP requests in a web
+/// application.</remarks>
+public interface IEndpoint
+{
+    /// <summary>
+    /// Maps the endpoint to the specified <see cref="IEndpointRouteBuilder"/>.
+    /// </summary>
+    /// <param name="app">The <see cref="IEndpointRouteBuilder"/> to which the endpoint will be mapped. Cannot be null.</param>
+    static abstract void MapEndpoint(IEndpointRouteBuilder app);
+}
---
+++ tests/API.Generators.UnitTests/GeneratorTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+using Tolitech.Presentation.API.Generators.UnitTests.Implementations.Endpoints;
+
+namespace Tolitech.Presentation.API.Generators.UnitTests;
+
+/// <summary>
+/// Contains unit tests for verifying the behavior of endpoint registration in a web application.
+/// </summary>
+public class GeneratorTests
+{
+    /// <summary>
+    /// Tests that the MapEndpoints method does not throw an exception.
+    /// </summary>
+    [Fact]
+    public void Endpoint_MapEndpoints_ShouldNotThrowsException()
+    {
+        // Arrange
+        var builder = WebApplication.CreateBuilder();
+        var app = builder.Build();
+
+        // Act
+        app.MapEndpoints();
+
+        // Assert
+        Assert.True(true);
+    }
+
+    /// <summary>
+    /// Tests that the <see cref="TestEndpoint.Handle"/> method returns a result of type <see cref="Results{T1, T2}"/>
+    /// where <c>T1</c> is <see cref="Ok"/> and <c>T2</c> is <see cref="BadRequest"/>.
+    /// </summary>
+    [Fact]
+    public void Endpoint_Handle_ShouldReturnsOkResult()
+    {
+        // Arrange && Act
+        var result = TestEndpoint.Handle();
+
+        // Assert
+        Assert.IsType<Results<Ok, BadRequest>>(result);
+    }
+}
